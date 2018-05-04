1c30e1278a3d036b977bd946d8709b2f95116761
kazumichi kuroda <kazumichi96@gmail.com>
2018-05-04 16:00:31 +0900

    added action for create history.

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,2 +1,13 @@
 class HistoriesController < ApplicationController
+  def create
+    @technology = Technology.find(params[:technology_id])
+
+    @technology.histories.create(history_params)
+    redirect_to technology_path(@technology)
+  end
+
+  private
+  def history_params
+    params.require(:history).permit(:title)
+  end
 end
