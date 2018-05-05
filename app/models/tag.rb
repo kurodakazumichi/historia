fb722347feb7261d3443efdbdd67b1dd070dadbb
kazumichi kuroda <kazumichi96@gmail.com>
2018-05-05 13:35:53 +0900

    fixed association #13

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,4 @@
 class Tag < ApplicationRecord
-  has_many: technologies, through: technology_tags
-  has_many: technology_tags, dependent: :destroy
+  has_many :technologies, through: :technology_tags
+  has_many :technology_tags, dependent: :destroy
 end
