d52535f0182c62dd00bbe5c6179d1502956e8408
kazumichi kuroda <kazumichi96@gmail.com>
2018-05-05 13:36:51 +0900

    added validates of tag. #13

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ApplicationRecord
   has_many :technologies, through: :technology_tags
   has_many :technology_tags, dependent: :destroy
+
+  validates :name, presence:true, length:{maximum:20}
 end
--- app/models/technology_tag.rb
+++ app/models/technology_tag.rb
@@ -1,4 +1,7 @@
 class TechnologyTag < ApplicationRecord
   belongs_to :technology
   belongs_to :tag
+
+  validates :technology_id, presence:true
+  validates :tag_id, presence:true
 end
