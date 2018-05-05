b27170ac1679359ddde51f460b34c38587dd0d4d
kazumichi kuroda <kazumichi96@gmail.com>
2018-05-05 17:46:45 +0900

    modified default order of tag. #13

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,5 +2,7 @@ class Tag < ApplicationRecord
   has_many :technologies, through: :technology_tags
   has_many :technology_tags, dependent: :destroy
 
+  default_scope -> { order(:name) }
+
   validates :name, presence:true, length:{maximum:20}
 end
