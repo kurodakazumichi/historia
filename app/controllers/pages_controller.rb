938a79602a164dead1dfdad8bf27845336ad4a9e
kazumichi kuroda <kazumichi96@gmail.com>
2018-05-05 11:49:07 +0900

    updated technologies table.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def index
+    @technologies = Technology.all.order(:year, :month, :day)
+    @histories = History.all.order(:year, :month, :day)
   end
 end
