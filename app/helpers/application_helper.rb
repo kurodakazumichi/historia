3774875437353f8359c9b3c873912c4c83a6ae5f
kazumichi kuroda <kazumichi96@gmail.com>
2018-05-06 17:49:39 +0900

    Fixed function of birthday, and age formatter. Becouse db schema modified. #14

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,30 @@
 module ApplicationHelper
 
   def date(y, m, d)
+    y = 0 if y.nil?
+    m = 0 if m.nil?
+    d = 0 if d.nil?
     s  = "#{y}年"
-    s += "#{m}月" if !m.empty?
-    s += "#{d}日" if !m.empty? && !d.empty?
-    s += "頃"     if m.empty? || d.empty?
+    s += "#{m}月" if (0 < m)
+    s += "#{d}日" if (0 < m) && (0 < d)
+    s += "頃"     if (m == 0) || (d == 0)
     return s
   end
 
   def age(year, month, day)
-    y = year.to_i
-    m = (month.empty?)? 1 : month.to_i
-    d = (day.empty?)?   1 : day.to_i
+    year = 0 if year.nil?
+    month = 0 if month.nil?
+    day = 0 if day.nil?    
+    y = year
+    m = (month == 0)? 1 : month
+    d = (day == 0)?   1 : day
     f = "%Y%m%d"
     age  = Date.today.strftime(f).to_i
     age -= Date.new(y, m, d).strftime(f).to_i
     age /= 10000
     age  = age.to_s
     age += "才"
-    age += "くらい" if month.empty? || day.empty?
+    age += "くらい" if (month == 0) || (day == 0)
     return age
   end
 
